715ade62fced9785a3494b926f01cd7a0756c202
Tanh101 <lytanh@starboard-asia.com>
2023-07-13 15:01:05 +0700

    migration and model

---
+++ database/migrations/2023_07_13_035051_update_table_users_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateTableUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('username');
+            $table->boolean('is_admin');
+            $table->boolean('is_active');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->dropColumn('name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn([
+                'username',
+                'is_admin',
+                'is_active',
+                'first_name',
+                'last_name',
+            ]);
+            $table->string('name');
+        });
+    }
+}
